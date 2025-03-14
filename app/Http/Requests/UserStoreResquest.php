23758b20a9f2040c3010b07ca765eed5cacd0c57
hamzalhadouchi <hamzabniss16@gmail.com>
2025-03-14 21:54:54 +0000

    crée user stor Requist pour valide les champe cote backend

--- app/Http/Requests/UserStoreResquest.php
+++ app/Http/Requests/UserStoreResquest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UserResquest extends FormRequest
+class UserStoreResquest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -19,11 +19,15 @@ class UserResquest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
-    public function rules(): array
+    public function rules()
     {
         return [
-            'nom_User' => 'required|min:3|max:255',
-            'prenom' => 'required|min:3|max:255'
+            'nom_Utilisateur' => 'required|string|max:255',
+            'prenom' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:utilisateurs',
+            'motDePasse' => 'required|string|min:8',
+            'telephone' => 'nullable|string|max:20',
+            'statut' => 'nullable|in:actif,inactif',
         ];
     }
 }
