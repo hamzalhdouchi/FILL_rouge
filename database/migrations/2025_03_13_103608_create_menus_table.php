d4c63cac07766044c8eb7759ad3fbe448b5295b5
hamzalhadouchi <hamzabniss16@gmail.com>
2025-03-15 21:06:07 +0000

    crée fillable de menu

--- database/migrations/2025_03_13_103608_create_menus_table.php
+++ database/migrations/2025_03_13_103608_create_menus_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('menus', function (Blueprint $table) {
-            $table->id();
+            $table->id(); 
+            $table->string('name_Menu');
+            $table->boolean('isActif')->default(true);
             $table->timestamps();
         });
     }
