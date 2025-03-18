f6024a7c79a9a4a6e36a8d2566dd523aa46552de
hamzalhadouchi <hamzabniss16@gmail.com>
2025-03-18 13:41:53 +0000

    crée service de restaurants

---
+++ app/Services/RestaurantService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\RestaurantRepositoryInterface;
+
+class RestaurantService implements RestaurantServiceInterface
+{
+    protected $restaurantRepository;
+
+    public function __construct(RestaurantRepositoryInterface $restaurantRepository)
+    {
+        $this->restaurantRepository = $restaurantRepository;
+    }
+
+    public function getAllRestaurants()
+    {
+        return $this->restaurantRepository->getAll();
+    }
+
+    public function getRestaurantById($id)
+    {
+        return $this->restaurantRepository->getById($id);
+    }
+
+    public function createRestaurant(array $data)
+    {
+        return $this->restaurantRepository->create($data);
+    }
+
+    public function updateRestaurant(array $data, $id)
+    {
+        return $this->restaurantRepository->update($data, $id);
+    }
+
+    public function deleteRestaurant($id)
+    {
+        return $this->restaurantRepository->delete($id);
+    }
+}
