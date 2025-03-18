206c1dc894a8fe0868c9bfc3df4d4a05f7709cf1
hamzalhadouchi <hamzabniss16@gmail.com>
2025-03-18 23:34:31 +0000

    crée categorie service

---
+++ app/Services/CategorieService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Categorie;
+use App\Services\Interfaces\CategorieServiceInterface;
+use App\RepositoryInterfaces\CategorieRepositoryInterface;
+
+class CategorieService implements CategorieServiceInterface
+{
+    protected $categorieRepository;
+
+    public function __construct(CategorieRepositoryInterface $categorieRepository)
+    {
+        $this->categorieRepository = $categorieRepository;
+    }
+
+    public function getAllCategories()
+    {
+        return $this->categorieRepository->getAll();
+    }
+
+    public function getCategoryById($id)
+    {
+        return $this->categorieRepository->findById($id);
+    }
+
+    public function createCategory(array $data)
+    {
+        return $this->categorieRepository->create($data);
+    }
+
+    public function updateCategory(Categorie $category, array $data)
+    {
+        return $this->categorieRepository->update($category, $data);
+    }
+
+    public function deleteCategory(Categorie $category)
+    {
+        return $this->categorieRepository->delete($category);
+    }
+}
