09059482b26caeee7251c8bba096eb0bb7bf449b
hamzalhadouchi <hamzabniss16@gmail.com>
2025-03-18 15:01:24 +0000

    crée menu repository

---
+++ app/Repositories/MenuRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Menu;
+use App\Models\Restaurant;
+use App\Repositories\Interfaces\MenuRepositoryInterface;
+
+class MenuRepository implements MenuRepositoryInterface
+{
+    public function getAllMenus($restaurantId)
+    {
+        $restaurant = Restaurant::find($restaurantId);
+        return $restaurant ? $restaurant->menus : null;
+    }
+
+    public function getMenuById($restaurantId, $menuId)
+    {
+        return Menu::where('idRestaurant', $restaurantId)->find($menuId);
+    }
+
+    public function createMenu($restaurantId, array $data)
+    {
+        $restaurant = Restaurant::find($restaurantId);
+        return $restaurant ? $restaurant->menus()->create($data) : null;
+    }
+
+    public function updateMenu($restaurantId, $menuId, array $data)
+    {
+        $menu = $this->getMenuById($restaurantId, $menuId);
+        return $menu ? tap($menu)->update($data) : null;
+    }
+
+    public function deleteMenu($restaurantId, $menuId)
+    {
+        $menu = $this->getMenuById($restaurantId, $menuId);
+        return $menu ? $menu->delete() : false;
+    }
+}
