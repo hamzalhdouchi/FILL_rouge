6777e7f9c3b3aa66f4739fbd301df504d6251f28
hamzalhadouchi <hamzabniss16@gmail.com>
2025-03-18 23:28:19 +0000

    crée categorie repository interface

---
+++ app/RepositoryInterfaces/CategorieRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories\Interfaces;
+
+use App\Models\Categorie;
+
+interface CategorieRepositoryInterface
+{
+    public function getAll();
+    public function findById($id);
+    public function create(array $data);
+    public function update(Categorie $category, array $data);
+    public function delete(Categorie $category);
+}
