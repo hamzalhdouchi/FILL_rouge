6eaaec217900cf0ea16bd6cc612b5d392b3c6f7b
hamzalhadouchi <hamzabniss16@gmail.com>
2025-03-18 15:03:48 +0000

    crée menu service

---
+++ app/Services/MenuService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Interfaces\MenuRepositoryInterface;
+use App\Services\Interfaces\MenuServiceInterface;
+
+class MenuService implements MenuServiceInterface
+{
+    protected $menuRepository;
+
+    public function __construct(MenuRepositoryInterface $menuRepository)
+    {
+        $this->menuRepository = $menuRepository;
+    }
+
+    public function getAllMenus($restaurantId)
+    {
+        return $this->menuRepository->getAllMenus($restaurantId);
+    }
+
+    public function getMenuById($restaurantId, $menuId)
+    {
+        return $this->menuRepository->getMenuById($restaurantId, $menuId);
+    }
+
+    public function createMenu($restaurantId, array $data)
+    {
+        return $this->menuRepository->createMenu($restaurantId, $data);
+    }
+
+    public function updateMenu($restaurantId, $menuId, array $data)
+    {
+        return $this->menuRepository->updateMenu($restaurantId, $menuId, $data);
+    }
+
+    public function deleteMenu($restaurantId, $menuId)
+    {
+        return $this->menuRepository->deleteMenu($restaurantId, $menuId);
+    }
+}
