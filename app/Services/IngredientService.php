bdc7a2e47ec7ad7e53d1d59bceebc7337e35b62f
hamzalhadouchi <hamzabniss16@gmail.com>
2025-03-19 00:37:50 +0000

    ajouter les methode sur file de service

---
+++ app/Services/IngredientService.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Interfaces\IngredientRepositoryInterface;
+
+class IngredientService
+{
+    protected $ingredientRepository;
+
+    public function __construct(IngredientRepositoryInterface $ingredientRepository)
+    {
+        $this->ingredientRepository = $ingredientRepository;
+    }
+
+    public function ajouterIngredient(array $data)
+    {
+        $ingredient = $this->ingredientRepository->ajouterIngredient($data);
+        return [
+            'message' => 'Ingrédient ajouté avec succès.',
+            'ingredient' => $ingredient
+        ];
+    }
+
+    public function afficherIngredient()
+    {
+        return $this->ingredientRepository->afficherIngredient();
+    }
+
+    public function modifierIngredient($id, array $data)
+    {
+        $ingredient = $this->ingredientRepository->modifierIngredient($id, $data);
+        if ($ingredient) {
+            return [
+                'message' => 'Ingrédient mis à jour avec succès.',
+                'ingredient' => $ingredient
+            ];
+        }
+        return ['message' => 'Ingrédient non trouvé.'];
+    }
+
+    public function supprimerIngredient($id)
+    {
+        $result = $this->ingredientRepository->supprimerIngredient($id);
+        if ($result) {
+            return ['message' => 'Ingrédient supprimé avec succès.'];
+        }
+        return ['message' => 'Ingrédient non trouvé.'];
+    }
+
+    public function mettreAJourStock($id, $quantite)
+    {
+        $ingredient = $this->ingredientRepository->mettreAJourStock($id, $quantite);
+        if ($ingredient) {
+            return [
+                'message' => 'Stock mis à jour avec succès.',
+                'ingredient' => $ingredient
+            ];
+        }
+        return ['message' => 'Ingrédient non trouvé.'];
+    }
+
+    public function verifierDisponibilite($id)
+    {
+        $ingredient = $this->ingredientRepository->verifierDisponibilite($id);
+        if ($ingredient) {
+            return [
+                'message' => 'Ingrédient trouvé.',
+                'ingredient' => $ingredient
+            ];
+        }
+        return ['message' => 'Ingrédient non trouvé.'];
+    }
+}
