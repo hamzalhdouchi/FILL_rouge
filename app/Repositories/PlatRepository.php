8463b6acfe4331f3bce22b0c8790e59c67694430
hamzalhadouchi <hamzabniss16@gmail.com>
2025-03-19 01:51:33 +0000

    crée le palt reposotery

---
+++ app/Repositories/PlatRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Plat;
+use App\RepositoryInterfaces\PlatRepositoryInterface;
+
+class PlatRepository implements PlatRepositoryInterface
+{
+    public function ajouterPlat(array $data)
+    {
+        return Plat::create($data);
+    }
+
+    public function affichePlats()
+    {
+        return Plat::all();
+    }
+
+    public function modifierPlat($id, array $data)
+    {
+        $plat = Plat::find($id);
+        if ($plat) {
+            $plat->update($data);
+            return $plat;
+        }
+        return null;
+    }
+
+    public function supprimerPlat($id)
+    {
+        $plat = Plat::find($id);
+        if ($plat) {
+            $plat->delete();
+            return true;
+        }
+        return false;
+    }
+
+    public function changerDisponibilite($id, $disponible)
+    {
+        $plat = Plat::find($id);
+        if ($plat) {
+            $plat->disponible = $disponible;
+            $plat->save();
+            return $plat;
+        }
+        return null;
+    }
+}
