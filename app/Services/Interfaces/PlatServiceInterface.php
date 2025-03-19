0ebe1dce984a93e10e8451ef87d17b66d36d7134
hamzalhadouchi <hamzabniss16@gmail.com>
2025-03-19 01:52:32 +0000

    crée le palt service interface

---
+++ app/Services/Interfaces/PlatServiceInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Services\Interfaces;
+
+interface PlatServiceInterface
+{
+    public function ajouterPlat(array $data);
+    public function affichePlats();
+    public function modifierPlat($id, array $data);
+    public function supprimerPlat($id);
+    public function changerDisponibilite($id, $disponible);
+}
