e656d34e80668c98cf3a94087375b7dca5e4e60d
hamzalhadouchi <hamzabniss16@gmail.com>
2025-03-23 14:19:10 +0000

    creation de commande repo

---
+++ app/Repositories/CommandeRepository.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Repositories;
+
+use App\RepositoryInterfaces\CommandeRepositoryInterface;
+use App\Models\Commande;
+
+class CommandeRepository implements CommandeRepositoryInterface
+{
+    public function create(array $data)
+    {
+        return Commande::create($data);
+    }
+
+    public function getById($id)
+    {
+        return Commande::findOrFail($id);
+    }
+
+    public function update($id, array $data)
+    {
+        $commande = Commande::findOrFail($id);
+        $commande->update($data);
+        return $commande;
+    }
+
+    public function delete($id)
+    {
+        $commande = Commande::findOrFail($id);
+        $commande->delete();
+        return true;
+    }
+
+    public function changeStatus($id, $status)
+    {
+        $commande = Commande::findOrFail($id);
+        $commande->statut = $status;
+        $commande->save();
+        return $commande;
+    }
+
+    public function calculateTotal($id)
+    {
+        $commande = Commande::findOrFail($id);
+        return $commande->quantite * $commande->prixTotal;
+    }
+
+    public function calculateSubTotal($id)
+    {
+        $commande = Commande::findOrFail($id);
+        return $commande->quantite * ($commande->prixTotal * 0.9); // Exemple avec 10% de réduction
+    }
+}
