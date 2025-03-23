29b65b25d9b140601c9855defd727513219d92e8
hamzalhadouchi <hamzabniss16@gmail.com>
2025-03-23 14:22:51 +0000

    crée commande interface

---
+++ app/Services/Interfaces/CommandeServiceInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Interfaces;
+
+interface CommandeServiceInterface
+{
+    public function passerCommande(array $data);
+    public function annulerCommande($id);
+    public function evaluerService($id, $note);
+    public function calculerTotal($id);
+    public function calculerSousTotal($id);
+    public function changerStatut($id, $statut);
+    public function genererFacture($id);
+}
