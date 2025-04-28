d156730210b7c27b3dab16b52025b01402f2ca0a
hamzalhadouchi <hamzabniss16@gmail.com>
2025-04-28 19:26:38 +0000

    ajouter le statistique service

---
+++ app/Services/StatistiqueService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\StatistiqueRepositoryInterface;
+
+class StatistiqueService implements StatistiqueServiceInterface
+{
+    protected $repository;
+
+    public function __construct(StatistiqueRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function totalUsers()
+    {
+        return $this->repository->getTotalUsers();
+    }
+
+    public function newUsersToday()
+    {
+        return $this->repository->getNewUsersToday();
+    }
+
+    public function totalAcceptedRestaurants()
+    {
+        return $this->repository->getTotalAcceptedRestaurants();
+    }
+
+    public function totalRejectedRestaurants()
+    {
+        return $this->repository->getTotalRejectedRestaurants();
+    }
+
+    public function totalReservations()
+    {
+        return $this->repository->getTotalReservations();
+    }
+
+    public function totalPrixCommandes()
+    {
+        return $this->repository->getTotalPrixCommandes();
+    }
+}
