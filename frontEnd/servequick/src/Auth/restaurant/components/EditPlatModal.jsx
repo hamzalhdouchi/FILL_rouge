dbc6f01815aff9c1f1ebc011408c6ebb38208ab1
hamzalhadouchi <hamzabniss16@gmail.com>
2025-05-01 18:24:35 +0000

    feat: pré-remplissage des champs du formulaire depuis le plat sélectionné

--- frontEnd/servequick/src/Auth/restaurant/components/EditPlatModal.jsx
+++ frontEnd/servequick/src/Auth/restaurant/components/EditPlatModal.jsx
@@ -15,3 +15,16 @@ const EditPlatModal = ({ open, onClose, plat, onUpdate, idPlate, ingredients }) 
 
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  useEffect(() => {
+    if (plat) {
+      setFormData({
+        nom_plat: plat.nom_plat || '',
+        desciption: plat.desciption || '',
+        prix: plat.prix || '',
+        image: null,
+        temps_Preparation: plat.temps_Preparation || '',
+        categorie_id: plat.categorie_id || '',
+        ingredients: plat.ingredients || [],
+      });
+    }
+  }, [plat]);
