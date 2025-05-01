eec1331ef33433f0232b728c96fb2f19bc37fd56
hamzalhadouchi <hamzabniss16@gmail.com>
2025-05-01 19:18:04 +0000

    Déclaration du composant TableModal avec props

--- frontEnd/servequick/src/Auth/restaurant/components/formTable.jsx
+++ frontEnd/servequick/src/Auth/restaurant/components/formTable.jsx
@@ -2,3 +2,9 @@ import React, { useState, useCallback, useEffect } from 'react';
 import axios from 'axios';
 import Swal from 'sweetalert2';
 import PropTypes from 'prop-types';
+const TABLE_STATUSES = [
+    { value: 'libre', label: 'Libre' },
+    { value: 'occupee', label: 'Occupée' },
+    { value: 'reservee', label: 'Réservée' },
+  ];
+  
