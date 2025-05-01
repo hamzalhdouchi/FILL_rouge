eca26bcaefab60bef028f5c21a604846d9907d18
hamzalhadouchi <hamzabniss16@gmail.com>
2025-05-01 19:07:08 +0000

    Initialisation du formulaire avec ou sans plat sélectionné

--- frontEnd/servequick/src/Auth/restaurant/components/formPlates.jsx
+++ frontEnd/servequick/src/Auth/restaurant/components/formPlates.jsx
@@ -12,3 +12,13 @@ export default function CreatePlatModal({ closeModal, selectedPlat, fetchPlats }
 
   const restaurants = JSON.parse(sessionStorage.getItem('restaurant'));
   const res_id = restaurants.id;
+  const [form, setForm] = useState({
+    nom_plat: selectedPlat ? selectedPlat.nom_plat : '',
+    desciption: selectedPlat ? selectedPlat.desciption : '',
+    prix: selectedPlat ? selectedPlat.prix : '',
+    temps_Preparation: selectedPlat ? selectedPlat.temps_Preparation : '',
+    image: null,
+    categorie_id: selectedPlat ? selectedPlat.categorie_id : '',
+    ingredients: selectedPlat ? selectedPlat.ingredients : [],
+    menu_id: parseInt(menu_id),
+  });
